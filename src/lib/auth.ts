075a5d3e16104207ecd9eda2b6c662ba1d8ca544
farjadakbar4 <farjadakbar4@gmail.com>
2023-08-23 16:21:30 +0500

    fix: postgress deployment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,7 +27,7 @@ export const authOptions: NextAuthOptions = {
           return {
             id: user.id,
             email: profile.email,
-            username : profile.username,
+            name: profile.name,
             randomKey: "Hey cool",
           };
         } else {
@@ -48,28 +48,25 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        if (!credentials?.email || !credentials.password) {
-          return null;
-        }
-
         const payload = {
           email: credentials?.email,
           password: credentials?.password,
         };
 
-        const user = await prisma.user.findUnique({
-          where: payload,
+        const res = await fetch("https://api.storerestapi.com/auth/login", {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-Type": "application/json",
+          },
         });
 
-        if (!user) {
-          throw new Error("User is not registered");
-        }
-
-        if (user && !user.isVerified) {
-          throw new Error("User is not verified");
+        const user = await res.json();
+        if (!res.ok) {
+          throw new Error(user.message);
         }
         // If no error and we have user data, return it
-        if (user && user.isVerified) {
+        if (res.ok && user) {
           return user;
         }
 
