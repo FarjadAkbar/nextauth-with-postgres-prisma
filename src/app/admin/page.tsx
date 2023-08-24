3123b78ca3b509deaf4d77d424aa5e4c783c6ec7
Yarpa Mehdi <yarpamehdi2000@gmail.com>
2023-08-24 21:27:46 +0500

    fix: routes fixes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,23 @@
-'use client'
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import Header from "@/components/header.component";
 import AdminPage from "@/components/payments/page";
-import React from "react";
 
-export default function Admin() {
-  return (
-    <>
-      <Header />
-      <section className="bg-ct-blue-600 min-h-screen ">
-        <div className="container">
-            <AdminPage />
-        </div>
-      </section>
-    </>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session == null) {
+    return redirect("/login");
+  } else {
+    return (
+      <>
+        <Header />
+        <section className="bg-ct-blue-600 min-h-screen ">
+          <div className="container">
+          <AdminPage />
+          </div>
+        </section>
+      </>
+    );
+  }
 }
